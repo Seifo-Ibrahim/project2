9ce58cdfa6a9b7085fc8825c01037ea76ed09153
seifoibrahim <seifoibrahim36@gmail.com>
2021-01-18 14:06:04 +0200

    first commit

---
+++ scripts/bootstarpcos.js
@@ -0,0 +1,12 @@
+
+//SmoothScroll
+var scroll = new SmoothScroll('a[href*="#"]');
+
+//headerfullscreen
+$(function(){
+    $('.header').height($(window).height());
+
+    $('.contact').height($(window).height());
+});
+
+
